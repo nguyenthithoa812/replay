cff4589e1f0223712b051eb38d7e3bbea9150e00
nguyenthithoa812 <40637622+nguyenthithoa812@users.noreply.github.com>
2018-06-28 07:54:21 +0700

    Create replay.php

---
+++ replay.php
@@ -0,0 +1,22 @@
+{
+   "data":[
+      {
+         "title_replay":"Xem l\u1ea1i: H\u00e0n Qu\u1ed1c vs \u0110\u1ee9c",
+         "replay_img":"http://tructiepbongda.com/hinh-anh/2018/06/full-match-Italy-vs-Netherlands.jpg",
+         "replay_url":"",
+         "replay_url2":""
+      },
+      {
+         "title_replay":"Xem l\u1ea1i: Iceland vs Croatia",
+         "replay_img":"http://tructiepbongda.com/hinh-anh/2018/06/full-match-Italy-vs-Netherlands.jpg",
+         "replay_url":"",
+         "replay_url2":""
+      },
+      {
+         "title_replay":"Xem l\u1ea1i: Nigeria vs Argentina",
+         "replay_img":"http://tructiepbongda.com/hinh-anh/2018/06/full-match-Italy-vs-Netherlands.jpg",
+         "replay_url":"",
+         "replay_url2":""
+      }
+   ]
+}
